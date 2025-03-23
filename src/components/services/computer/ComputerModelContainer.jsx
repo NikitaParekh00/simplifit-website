ab967914dcdc2316b663632f103961d9ec413139
Nikita <nikita.parekh@nupipay.com>
2025-03-24 00:14:39 +0530

    dlb added for fitness

--- src/components/services/computer/ComputerModelContainer.jsx
+++ src/components/services/computer/ComputerModelContainer.jsx
@@ -10,7 +10,7 @@ const ComputerModelContainer = () => {
         <Stage environment="night" intensity={0.5}>
           <ComputerModel />
         </Stage>
-        <OrbitControls enableZoom={true} autoRotate />
+        <OrbitControls enableZoom={true} />
         <PerspectiveCamera position={[-1, 0, 1.8]} zoom={0.8} makeDefault />
       </Suspense>
     </Canvas>
